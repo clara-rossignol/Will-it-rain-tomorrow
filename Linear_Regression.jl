51c83b9c3fb863e31671241bdd8ef9b0afdb89c7
Oscar-Henry <h.oscar78515@gmail.com>
2021-12-19 20:27:44 +0100

    New

--- Linear_Regression.jl
+++ Linear_Regression.jl
@@ -5,14 +5,6 @@ import GLMNet: glmnet
 
 example_submission = CSV.read(joinpath(@__DIR__, "data", "sample_submission.csv"), DataFrame);
 
-train = CSV.read(joinpath(@__DIR__, "data","train.csv"), DataFrame)
-valid = CSV.read(joinpath(@__DIR__, "data","valid.csv"), DataFrame)
-test = CSV.read(joinpath(@__DIR__, "data","test.csv"), DataFrame)
-
-coerce!(train, :precipitation_nextday => Multiclass);
-coerce!(valid, :precipitation_nextday => Multiclass);
-coerce!(test, :precipitation_nextday => Multiclass);
-
 m_logistic = machine(LogisticClassifier(penalty = :l2, lambda = 2e-2), select(train[:,:], Not([:precipitation_nextday])), train.precipitation_nextday) |> fit!;
 
 # should put functions in a function file 
@@ -37,8 +29,6 @@ end
 
 res1_lasso = fitted_params(tune_model(LogisticClassifier(), train))
 
-"""losses(m_logistic, select(test[:,:], Not([:precipitation_nextday])), test.precipitation_nextday)"""
+losses(m_logistic, select(test[:,:], Not([:precipitation_nextday])), test.precipitation_nextday)
 
-"""confusion_matrix(predict_mode(m_logistic, select(test[:,:], Not([:precipitation_nextday]))), test.precipitation_nextday)"""
-
-
+confusion_matrix(predict_mode(m_logistic, select(test[:,:], Not([:precipitation_nextday]))), test.precipitation_nextday)
