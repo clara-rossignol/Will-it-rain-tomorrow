4762341fe0cc596776c4ced198b71ead3370b2ee
Clara Rossignol <84544110+clara-rossignol@users.noreply.github.com>
2021-12-19 18:27:05 +0100

    logistic classifier

--- logistic_classifier.jl
+++ logistic_classifier.jl
@@ -1,36 +1,19 @@
 using Pkg
 Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 using Plots, OpenML, CSV, StatsPlots, MLJ, DataFrames, MLJLinearModels, Random, MLJLIBSVMInterface, NearestNeighborModels
+import GLMNet: glmnet
 
-weather = CSV.read(joinpath(@__DIR__, "data","trainingdata.csv"), DataFrame)
-test = CSV.read(joinpath(@__DIR__, "data", "testdata.csv"), DataFrame);
 example_submission = CSV.read(joinpath(@__DIR__, "data", "sample_submission.csv"), DataFrame);
 
-size(weather)
-describe(weather)
+train = CSV.read(joinpath(@__DIR__, "data","train.csv"), DataFrame)
+valid = CSV.read(joinpath(@__DIR__, "data","valid.csv"), DataFrame)
+test = CSV.read(joinpath(@__DIR__, "data","test.csv"), DataFrame)
 
-weather_no_missing = dropmissing(weather)
-size(weather_no_missing)
+coerce!(train, :precipitation_nextday => Multiclass);
+coerce!(valid, :precipitation_nextday => Multiclass);
+coerce!(test, :precipitation_nextday => Multiclass);
 
-"""
-Here it is not a good idea to use the function dropmissing (removes rows with missing values) 
-as it's removing almost half of the data !
-We use FillIputer, which imputes missing data with a fixed value computed on the non-missing 
-values. --> Try to change which value it computes
-"""
-
-imputed_data = MLJ.transform(fit!(machine(FillImputer(), select(weather[:,:], Not([:precipitation_nextday])))), select(weather[:,:], Not([:precipitation_nextday])));
-
-data = insertcols!(imputed_data,:precipitation_nextday=>weather.precipitation_nextday[:]);
-
-coerce!(data, :precipitation_nextday => Multiclass);
-
-train_input = select(data[1:3000,:], Not(:precipitation_nextday))
-train_output = data.precipitation_nextday[1:3000]
-test_input = select(data[3001:3176,:], Not(:precipitation_nextday))
-test_output = data.precipitation_nextday[3001:3176]
-
-m_logistic = machine(LogisticClassifier(penalty = :l2, lambda = 2e-2), train_input, train_output) |> fit!;
+m_logistic = machine(LogisticClassifier(penalty = :l2, lambda = 2e-2), select(train[:,:], Not([:precipitation_nextday])), train.precipitation_nextday) |> fit!;
 
 # should put functions in a function file 
 function losses(machine, input, response)
@@ -41,8 +24,21 @@ function losses(machine, input, response)
 	)
 end;
 
-losses(m_logistic, data[3001:3176,:], test_output)
+function tune_model(model, data)
+	tuned_model = TunedModel(model = model,
+	                         resampling = CV(nfolds = 20),
+	                         tuning = Grid(goal = 200),
+	                         range = range(model, :lambda,
+									       scale = :log,
+									       lower = 1e-3, upper = 1e2),
+	                         measure = auc)
+                             machine(LogisticClassifier(penalty = :l2, lambda = 2e-2), select(train[:,:], Not([:precipitation_nextday])), train.precipitation_nextday) |> fit!;
+end
 
-confusion_matrix(predict_mode(m_logistic, test_input), test_output)
+res1_lasso = fitted_params(tune_model(LogisticClassifier(), train))
+
+"""losses(m_logistic, select(test[:,:], Not([:precipitation_nextday])), test.precipitation_nextday)"""
+
+"""confusion_matrix(predict_mode(m_logistic, select(test[:,:], Not([:precipitation_nextday]))), test.precipitation_nextday)"""
 
 
