066c988d9595cdb499a6837facbcb830a69be73c
Clara Rossignol <84544110+clara-rossignol@users.noreply.github.com>
2021-12-20 09:54:07 +0100

    function knn tuned model

--- knn_classifier.jl
+++ knn_classifier.jl
@@ -1,6 +1,6 @@
 using Pkg
 Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using Plots, OpenML, CSV, StatsPlots, MLJ, DataFrames, MLJLinearModels, Random, MLJLIBSVMInterface, NearestNeighborModels
+using Plots, CSV, MLJ, DataFrames, MLJLinearModels, Random, NearestNeighborModels
 
 weather = CSV.read(joinpath(@__DIR__, "data","trainingdata.csv"), DataFrame)
 test = CSV.read(joinpath(@__DIR__, "data", "testdata.csv"), DataFrame);
@@ -10,35 +10,41 @@ imputed_data = MLJ.transform(fit!(machine(FillImputer(), select(weather[:,:], No
 data = insertcols!(imputed_data,:precipitation_nextday=>weather.precipitation_nextday[:]);
 coerce!(data, :precipitation_nextday => Multiclass);
 
-train_input = select(data[1:3000,:], Not(:precipitation_nextday))
-train_output = data.precipitation_nextday[1:3000]
-test_input = select(data[3001:3176,:], Not(:precipitation_nextday))
-test_output = data.precipitation_nextday[3001:3176]
+idxs = randperm(size(data, 1))
+rows = size(data)[1]
+indices = Vector{Int}([1,floor(rows/10),floor(2*rows/10),floor(rows)])
 
-m_KNN = machine(KNNClassifier(K = 36), train_input, train_output) |>fit!
+train = data[idxs[indices[1]:indices[2]], :]
+valid = data[idxs[indices[2]:indices[3]], :]
+test = data[idxs[indices[3]:indices[4]], :]
 
-err = mean(predict_mode(m_KNN, test_input) .!= test_output)
-
-auc_KNN = MLJ.auc(predict(m_KNN, test_input), test_output)
-
-pred = pdf.(predict(m_KNN, test), true)
-
-example_submission.precipitation_nextday = pred
-
-CSV.write(joinpath(@__DIR__,"submission.csv"), example_submission)
+train_input = select(train[:,:], Not(:precipitation_nextday))
+train_output = train.precipitation_nextday
+test_input = select(test[:,:], Not(:precipitation_nextday))
+test_output = test.precipitation_nextday
 
 
-model_KNN = KNNClassifier()
-self_tuning_model = TunedModel(model = model_KNN,
-                                resampling = CV(nfolds = 5),
+function TunedModel_KNN(data)
+    model_KNN = KNNClassifier()
+    self_tuning_model = TunedModel(model = model_KNN,
+                                resampling = CV(nfolds = 20),
                                 tuning = Grid(),
-                                range = range(model_KNN, :K, values = 1:50),
+                                range = range(model_KNN, :K, values = 1:100),
                                 measure = auc)
-self_tuning_mach = machine(self_tuning_model,
+    self_tuning_mach = machine(self_tuning_model,
                             select(data, Not(:precipitation_nextday)),
                             data.precipitation_nextday) |> fit!
+    
+    #scatter(reshape(rep.plotting.parameter_values, :),
+	    #rep.plotting.measurements, xlabel = "K", ylabel = "AUC")
+    report(self_tuning_mach).best_model.K
+end
 
-rep = report(self_tuning_mach)
+best_KNN_machine = machine(KNNClassifier(K = TunedModel_KNN(train)), train_input, train_output) |>fit!
+auc_KNN = MLJ.auc(predict(best_KNN_machine, test_input), test_output)
 
-scatter(reshape(rep.plotting.parameter_values, :),
-	    rep.plotting.measurements, xlabel = "K", ylabel = "AUC")
+""" To write in the submission file
+pred = pdf.(predict(m_KNN, test), true)
+example_submission.precipitation_nextday = pred
+CSV.write(joinpath(@__DIR__,"submission.csv"), example_submission)
+"""
