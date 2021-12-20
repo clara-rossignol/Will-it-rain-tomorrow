b43283ffc70cb4f4d9f9102effb748a4824d0c4a
Oscar-Henry <h.oscar78515@gmail.com>
2021-12-20 16:28:37 +0100

    new

--- Data_Processing.jl
+++ Data_Processing.jl
@@ -2,10 +2,7 @@ using Pkg
 Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 using CSV, MLJ, DataFrames
 
-weather = CSV.read(joinpath(@__DIR__, "data","trainingdata.csv"), DataFrame);
-describe(weather)
-
-function generate(data)
+function generate(data, valid = "true")
     data_drop = dropmissing(data);
     data_med = MLJ.transform(fit!(machine(FillImputer(), select(data[:,:], Not([:precipitation_nextday])))), select(data[:,:], Not([:precipitation_nextday])));
     insertcols!(data_med,:precipitation_nextday=>data.precipitation_nextday[:]);
@@ -15,22 +12,36 @@ function generate(data)
 
     data_drop_std = data_drop;
     data_med_std = data_med;
-
+    Random.seed!(2809)
     idxs1 = randperm(size(data_drop, 1));
     idxs2 = randperm(size(data_med, 1));
     dim_drop = size(data_drop)[1];
     dim_med = size(data_med)[1];
 
-    idx_drop = Vector{Int}([1, floor(dim_drop/10), floor(2*dim_drop/10), dim_drop]);
+    idx_drop = Vector{Int}([1, floor(dim_drop/3), floor(2*dim_drop/3), dim_drop]);
     #1 / 169 / 339 / 1699
-    idx_med = Vector{Int}([1, floor(dim_med/10), floor(2*dim_med/10), dim_med]);
+    idx_med = Vector{Int}([1, floor(dim_med/3), floor(2*dim_med/3), dim_med]);
     #1 / 317 / 653 / 3176
-
-    drop = (train = data_drop[idxs1[idx_drop[1]:idx_drop[2]], :], valid = data_drop[idxs1[idx_drop[2]:idx_drop[3]], :], test = data_drop[idxs1[idx_drop[3]:idx_drop[4]], :]);
-    drop_std = (train = data_drop_std[idxs1[idx_drop[1]:idx_drop[2]], :], valid = data_drop_std[idxs1[idx_drop[2]:idx_drop[3]], :], test = data_drop_std[idxs1[idx_drop[3]:idx_drop[4]], :]);
-    med = (train = data_med[idxs2[idx_med[1]:idx_med[2]], :], valid = data_med[idxs2[idx_med[2]:idx_med[3]], :], test = data_med[idxs2[idx_med[3]:idx_med[4]], :]);
-    med_std = (train = data_med_std[idxs2[idx_med[1]:idx_med[2]], :], valid = data_med_std[idxs2[idx_med[2]:idx_med[3]], :], test = data_med_std[idxs2[idx_med[3]:idx_med[4]], :]);
-    drop, drop_std, med, med_std
+    
+    if valid == "true"
+        drop = (train = data_drop[idxs1[idx_drop[1]:idx_drop[2]], :], valid = data_drop[idxs1[idx_drop[2]:idx_drop[3]], :], test = data_drop[idxs1[idx_drop[3]:idx_drop[4]], :]);
+        drop_std = (train = data_drop_std[idxs1[idx_drop[1]:idx_drop[2]], :], valid = data_drop_std[idxs1[idx_drop[2]:idx_drop[3]], :], test = data_drop_std[idxs1[idx_drop[3]:idx_drop[4]], :]);
+        med = (train = data_med[idxs2[idx_med[1]:idx_med[2]], :], valid = data_med[idxs2[idx_med[2]:idx_med[3]], :], test = data_med[idxs2[idx_med[3]:idx_med[4]], :]);
+        med_std = (train = data_med_std[idxs2[idx_med[1]:idx_med[2]], :], valid = data_med_std[idxs2[idx_med[2]:idx_med[3]], :], test = data_med_std[idxs2[idx_med[3]:idx_med[4]], :]);
+        drop, drop_std, med, med_std
+    elseif valid == "false"
+        drop = (train = data_drop[idxs1[idx_drop[1]:idx_drop[3]], :], test = data_drop[idxs1[idx_drop[3]:idx_drop[4]], :]);
+        drop_std = (train = data_drop_std[idxs1[idx_drop[1]:idx_drop[3]], :], test = data_drop_std[idxs1[idx_drop[3]:idx_drop[4]], :]);
+        med = (train = data_med[idxs2[idx_med[1]:idx_med[3]], :], test = data_med[idxs2[idx_med[3]:idx_med[4]], :]);
+        med_std = (train = data_med_std[idxs2[idx_med[1]:idx_med[3]], :], test = data_med_std[idxs2[idx_med[3]:idx_med[4]], :]);
+        drop, drop_std, med, med_std
+    end
 end
 
-#drop, drop_std, med, med_std = generate(weather);
+function losses(machine, input, response)
+    (loglikelihood = -sum(log_loss(predict(machine, input), response)),
+     misclassification_rate = mean(predict_mode(machine, input) .!= response),
+     accuracy = accuracy(predict_mode(machine, input), response),
+     auc = MLJ.auc(predict(machine, input), response)
+	)
+end
--- knn_classifier.jl
+++ knn_classifier.jl
@@ -1,49 +1,37 @@
 using Pkg
 Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 using Plots, CSV, MLJ, DataFrames, MLJLinearModels, Random, NearestNeighborModels
+include("./Data_Processing.jl")
 
-weather = CSV.read(joinpath(@__DIR__, "data","trainingdata.csv"), DataFrame)
-test = CSV.read(joinpath(@__DIR__, "data", "testdata.csv"), DataFrame);
+drop, drop_std, med, med_std = generate(weather, "false");
 example_submission = CSV.read(joinpath(@__DIR__, "data", "sample_submission.csv"), DataFrame);
-
-imputed_data = MLJ.transform(fit!(machine(FillImputer(), select(weather[:,:], Not([:precipitation_nextday])))), select(weather[:,:], Not([:precipitation_nextday])));
-data = insertcols!(imputed_data,:precipitation_nextday=>weather.precipitation_nextday[:]);
-coerce!(data, :precipitation_nextday => Multiclass);
-
-idxs = randperm(size(data, 1))
-rows = size(data)[1]
-indices = Vector{Int}([1,floor(rows/10),floor(2*rows/10),floor(rows)])
-
-train = data[idxs[indices[1]:indices[2]], :]
-valid = data[idxs[indices[2]:indices[3]], :]
-test = data[idxs[indices[3]:indices[4]], :]
-
-train_input = select(train[:,:], Not(:precipitation_nextday))
-train_output = train.precipitation_nextday
-test_input = select(test[:,:], Not(:precipitation_nextday))
-test_output = test.precipitation_nextday
-
 
 function TunedModel_KNN(data)
     model_KNN = KNNClassifier()
-    self_tuning_model = TunedModel(model = model_KNN,
-                                resampling = CV(nfolds = 20),
+    self_tuning_model = TunedModel(model = KNNClassifier(),
+                                resampling = CV(nfolds = 10),
                                 tuning = Grid(),
                                 range = range(model_KNN, :K, values = 1:100),
                                 measure = auc)
     self_tuning_mach = machine(self_tuning_model,
-                            select(data, Not(:precipitation_nextday)),
-                            data.precipitation_nextday) |> fit!
-    
-    #scatter(reshape(rep.plotting.parameter_values, :),
-	    #rep.plotting.measurements, xlabel = "K", ylabel = "AUC")
-    report(self_tuning_mach).best_model.K
+                            select(data.train, Not(:precipitation_nextday)),
+                            data.train.precipitation_nextday) |> fit!
+    self_tuning_mach
 end
 
-best_KNN_machine = machine(KNNClassifier(K = TunedModel_KNN(train)), train_input, train_output) |>fit!
-auc_KNN = MLJ.auc(predict(best_KNN_machine, test_input), test_output)
+rep = report(TunedModel_KNN(drop))
+scatter(reshape(rep.plotting.parameter_values, :), rep.plotting.measurements, xlabel = "Lamba", ylabel = "AUC")
+rep.best_model
+best_mach = machine(KNNClassifier(K = rep.best_model.K), select(drop.train, Not(:precipitation_nextday)), drop.train.precipitation_nextday)|> fit!
+losses(best_mach, select(drop.test[:,:], Not(:precipitation_nextday)), drop.test.precipitation_nextday)
 
-""" To write in the submission file
+"""
+best_KNN_machine1 = machine(KNNClassifier(K = TunedModel_KNN(drop.train)), select(drop.train[:,:], Not(:precipitation_nextday)), drop.train.precipitation_nextday) |>fit!
+best_KNN_machine2 = machine(KNNClassifier(K = TunedModel_KNN(med.train)), select(drop.train[:,:], Not(:precipitation_nextday)), drop.train.precipitation_nextday) |>fit!
+auc_KNN = MLJ.auc(predict(best_KNN_machine1, select(drop.test[:,:], Not(:precipitation_nextday))), drop.test.precipitation_nextday)
+auc_KNN = MLJ.auc(predict(best_KNN_machine2, select(med.test[:,:], Not(:precipitation_nextday))), med.test.precipitation_nextday)
+
+To write in the submission file
 pred = pdf.(predict(m_KNN, test), true)
 example_submission.precipitation_nextday = pred
 CSV.write(joinpath(@__DIR__,"submission.csv"), example_submission)
