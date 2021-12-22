3a53d92135cf3d9b02950f257a0a2d8ce86bb2a1
Clara Rossignol <84544110+clara-rossignol@users.noreply.github.com>
2021-12-22 01:32:55 +0100

    New version

--- Tree-Based_Classifiers.jl
+++ Tree-Based_Classifiers.jl
@@ -5,13 +5,14 @@ include("./Data_Processing.jl")
 
 train_data = CSV.read(joinpath(@__DIR__, "data", "trainingdata.csv"), DataFrame);
 test_data = CSV.read(joinpath(@__DIR__, "data", "testdata.csv"), DataFrame);
-coerce!(train_data, :precipitation_nextday => Multiclass);
-coerce!(test_data, :precipitation_nextday => Multiclass);
-
 example_submission = CSV.read(joinpath(@__DIR__, "data", "sample_submission.csv"), DataFrame);
+coerce!(train_data, :precipitation_nextday => Multiclass);
+coerce!(test_data, Count => Continuous);
+
 
 drop = generate(train_data, option = "drop", valid = "false", test = "true");
 med = generate(train_data, option = "med", valid = "false", test = "true");
+
 
 
 #-------------------------------------------------------------------------------#
@@ -25,65 +26,114 @@ auc_forest_med = MLJ.auc(predict(m_forest_med, select(med.test[:,:], Not(:precip
 
 #med seems to have the best AUC
 
-function TunedModel_forest(data)
-    model_forest = RandomForestClassifier()
-    self_tuning_model = TunedModel(model = model_forest,
+function TunedModel_forest(data,model)
+    model = RandomForestClassifier()
+    self_tuning_model = TunedModel(model = model,
                                 resampling = CV(nfolds = 6),
                                 tuning = Grid(),
-                                range = range(model_forest, :n_trees, lower = 100, upper = 1000, scale = :log),
+                                range = range(model, :n_trees, lower = 100, upper = 1000, scale = :log),
                                 measure = auc)
     self_tuning_mach = machine(self_tuning_model,
-                            select(data.train, Not(:precipitation_nextday)),
-                            data.train.precipitation_nextday) |> fit!
+                            select(data, Not(:precipitation_nextday)),
+                            data.precipitation_nextday) |> fit!
     self_tuning_mach
 end
 
-#rep1 = report(TunedModel_forest(drop))
-rep2 = report(TunedModel_forest(med))
-#rep1.best_history_entry.measurement
+model = RandomForestClassifier()
+
+train_data = CSV.read(joinpath(@__DIR__, "data", "trainingdata.csv"), DataFrame);
+drop = generate(train_data, option = "drop", valid = "false", test = "true");
+rep1 = report(TunedModel_forest(drop.train, model))
+rep1.best_history_entry.measurement
+rep1.best_model.n_trees
+drop
+
+train_data = CSV.read(joinpath(@__DIR__, "data", "trainingdata.csv"), DataFrame);
+med = generate(train_data, option = "med", valid = "false", test = "true");
+rep2 = report(TunedModel_forest(med.train, model))
 rep2.best_history_entry.measurement
+rep2.best_model.n_trees
+med
 
-med_all = generate_all(train_data, "med");
-best_mach = machine(RandomForestClassifier(n_trees = rep2.best_model.n_trees), select(med_all[:,:], Not(:precipitation_nextday)), med_all.precipitation_nextday)|> fit!
+best_mach1 = machine(RandomForestClassifier(n_trees = rep1.best_model.n_trees), select(drop.train[:,:], Not(:precipitation_nextday)), drop.train.precipitation_nextday)|> fit!
+best_mach2 = machine(RandomForestClassifier(n_trees = rep2.best_model.n_trees), select(med.train[:,:], Not(:precipitation_nextday)), med.train.precipitation_nextday)|> fit!
 
-#To write in the submission file
+losses(best_mach1, select(drop.test, Not(:precipitation_nextday)), drop.test.precipitation_nextday)
+losses(best_mach2, select(med.test, Not(:precipitation_nextday)), med.test.precipitation_nextday)
+
+tot = generate(train_data, option = "med", valid = "false", test = "false");
+tot
+best_mach = machine(RandomForestClassifier(n_trees = rep2.best_model.n_trees), select(tot[:,:], Not(:precipitation_nextday)), tot.precipitation_nextday) |> fit!
+
+#To 
 pred = pdf.(predict(best_mach, test_data), true)
 example_submission.precipitation_nextday = pred
 CSV.write(joinpath(@__DIR__,"RandomForest_submission.csv"), example_submission)
 
 
 
+
 #-------------------------------------------------------------------------------#
 #XGBoostClassifier
 
-function TunedModel_XGB(data)
-    model_xgb = XGBoostClassifier()
-    self_tuning_model = TunedModel(model = model_xgb,
+function TunedModel_XGB(data,model)
+    model = XGBoostClassifier()
+    self_tuning_model = TunedModel(model = model,
                                 resampling = CV(nfolds = 6),
                                 tuning = Grid(goal = 20),
-                                range = [range(model_xgb, :eta,
+                                range = [range(model, :eta,
                                         lower = 1e-4, upper = .1, scale = :log),
-                                 range(model_xgb, :num_round, lower = 50, upper = 500),
-                                 range(model_xgb, :max_depth, lower = 2, upper = 6)],
+                                 range(model, :num_round, lower = 50, upper = 500),
+                                 range(model, :max_depth, lower = 2, upper = 6)],
                                 measure = auc)
     self_tuning_mach = machine(self_tuning_model,
-                            select(data.train, Not(:precipitation_nextday)),
-                            data.train.precipitation_nextday) |> fit!
+                            select(data, Not(:precipitation_nextday)),
+                            data.precipitation_nextday) |> fit!
     self_tuning_mach
 end
 
-#better with med than drop
-rep1 = report(TunedModel_XGB(drop))
-rep2 = report(TunedModel_XGB(med))
+
+model = XGBoostClassifier()
+
+train_data = CSV.read(joinpath(@__DIR__, "data", "trainingdata.csv"), DataFrame);
+drop = generate(train_data, option = "drop", valid = "false", test = "true");
+rep1 = report(TunedModel_XGB(drop.train, model))
 rep1.best_history_entry.measurement
+
+train_data = CSV.read(joinpath(@__DIR__, "data", "trainingdata.csv"), DataFrame);
+med = generate(train_data, option = "med", valid = "false", test = "true");
+rep2 = report(TunedModel_XGB(med.train, model))
 rep2.best_history_entry.measurement
 
 
-med_all = generate_all(train_data, "med");
-best_mach = machine(XGBoostClassifier(eta = rep2.best_model.eta, num_round = rep2.best_model.num_round, max_depth = rep2.best_model.max_depth), 
-                    select(med_all[:,:], Not(:precipitation_nextday)), med_all.precipitation_nextday)|> fit!
+best_mach1 = machine(XGBoostClassifier(eta = rep1.best_model.eta, num_round = rep1.best_model.num_round, max_depth = rep1.best_model.max_depth), select(drop.train[:,:], Not(:precipitation_nextday)), drop.train.precipitation_nextday)|> fit!
+best_mach2 = machine(XGBoostClassifier(eta = rep2.best_model.eta, num_round = rep2.best_model.num_round, max_depth = rep2.best_model.max_depth), select(med.train[:,:], Not(:precipitation_nextday)), med.train.precipitation_nextday)|> fit!
 
-#To write in the submission file
+losses(best_mach1, select(drop.test, Not(:precipitation_nextday)), drop.test.precipitation_nextday)
+losses(best_mach2, select(med.test, Not(:precipitation_nextday)), med.test.precipitation_nextday)
+
+tot = generate(train_data, option = "med", valid = "false", test = "false");
+tot
+best_mach = machine(XGBoostClassifier(eta = rep2.best_model.eta, num_round = rep2.best_model.num_round, max_depth = rep2.best_model.max_depth), select(tot[:,:], Not(:precipitation_nextday)), tot.precipitation_nextday)|> fit!
+
+#To 
 pred = pdf.(predict(best_mach, test_data), true)
 example_submission.precipitation_nextday = pred
-CSV.write(joinpath(@__DIR__,"xgb_submission.csv.csv"), example_submission)
+CSV.write(joinpath(@__DIR__,"XGB_submission.csv"), example_submission)
+
+
+
+#test
+model = XGBoostClassifier()
+self_tuning_model = TunedModel(model = model,
+                                resampling = CV(nfolds = 20),
+                                tuning = Grid(goal = 200),
+                                range = [range(model, :eta,
+                                        lower = 1e-4, upper = .1, scale = :log),
+                                 range(model, :num_round, lower = 50, upper = 500),
+                                 range(model, :max_depth, lower = 2, upper = 6)],
+                                measure = auc)
+self_tuning_mach = machine(self_tuning_model,
+                            select(data, Not(:precipitation_nextday)),
+                            data.precipitation_nextday) |> fit!
+self_tuning_mach
