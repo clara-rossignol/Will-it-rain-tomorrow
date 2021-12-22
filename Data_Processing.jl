08bbcd5d05ab46eb76849fb7e707e4b0a2dce9df
Clara Rossignol <84544110+clara-rossignol@users.noreply.github.com>
2021-12-22 11:04:26 +0100

    Fixed error with the data processing

--- Data_Processing.jl
+++ Data_Processing.jl
@@ -1,14 +1,13 @@
 using Pkg
 Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using CSV, MLJ, DataFrames
+using CSV, MLJ, DataFrames, Random
 
 function generate(data; option = "drop", valid = "true", test = "true")
     new_data = data
     Random.seed!(2809)
     
     if option == "drop"
-        dropmissing!(new_data);
-        coerce!(new_data, Count => Continuous);
+        new_data = dropmissing!(copy(data));
         coerce!(new_data, :precipitation_nextday => Multiclass);
         idxs = randperm(size(new_data, 1));
         dim = size(new_data)[1];
@@ -19,7 +18,7 @@ function generate(data; option = "drop", valid = "true", test = "true")
                 new_data = (train = new_data[idxs[idx_valid[1]:idx_valid[2]], :], valid = new_data[idxs[idx_valid[2]:idx_valid[3]], :], test = new_data[idxs[idx_valid[3]:idx_valid[4]], :]);
                 new_data
             else
-                "We do not provide validation set without a test set, look at your arg."
+                "We do not provide a validation set without a test set, look at your arg."
                 empty = DataFrame()
                 empty
             end
@@ -35,19 +34,18 @@ function generate(data; option = "drop", valid = "true", test = "true")
         end
     elseif option == "med"
         new_data = MLJ.transform(fit!(machine(FillImputer(), select(data, Not(:precipitation_nextday)))), select(data, Not(:precipitation_nextday)));
-        new_data = insertcols!(new_data,:precipitation_nextday=>data.precipitation_nextday[:]);
-        coerce!(new_data, Count => Continuous);
+        insertcols!(new_data,:precipitation_nextday=>data.precipitation_nextday[:]);
         coerce!(new_data, :precipitation_nextday => Multiclass);
         idxs = randperm(size(new_data, 1));
         dim = size(new_data)[1];
         if valid == "true"
             if test == "true"
-                #If valid is true then a test set is also creater
+                #If valid is true then a test set is also created
                 idx_valid = Vector{Int}([1, floor(dim/3), floor(2*dim/3), dim]);
                 new_data = (train = new_data[idxs[idx_valid[1]:idx_valid[2]], :], valid = new_data[idxs[idx_valid[2]:idx_valid[3]], :], test = new_data[idxs[idx_valid[3]:idx_valid[4]], :]);
                 new_data
             else
-                "We do not provide validation set without a test set, look at your arg."
+                "We do not provide a validation set without a test set, look at your arg."
                 empty = DataFrame()
                 empty
             end
@@ -68,6 +66,5 @@ function losses(machine, input, response)
     (loglikelihood = -sum(log_loss(predict(machine, input), response)),
      misclassification_rate = mean(predict_mode(machine, input) .!= response),
      accuracy = accuracy(predict_mode(machine, input), response),
-     auc = MLJ.auc(predict(machine, input), response)
-	)
+     auc = MLJ.auc(predict(machine, input), response))
 end
